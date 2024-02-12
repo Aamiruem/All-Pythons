93a5adff6739a607c746e4c74caabcca81d84f9f
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-02-12 13:50:15 +0530

    commit template

---
+++ Python Course with Notes (3)/2. Chapter 2/01_variable.py
@@ -0,0 +1,20 @@
+a_122 = '''Aamir'''
+a: str = 'Kamran'
+a1 = "harry"
+b = 345
+c = 45.34
+d = True
+e = None
+
+# Printing the variables
+print(a1)
+print(a_122)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(a_122))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ Python Course with Notes (3)/3. Chapter 3/02_string_slicing.py
@@ -0,0 +1,21 @@
+greeting = "Good Morning, "
+name = "Aamir"
+print(type(name))
+
+# Concatenating two strings
+c = greeting + name
+print(c)
+name: str = "Aamir"
+print(name[4])
+# name[3] = "d" --> Does not work
+
+print(name[1:4])
+print(name[:4]) # is same as name[0:4]
+print(name[1:]) # is same as name[1:5]
+c = name[-4:-1] # is same is name[1:4]
+print(c)
+
+name1 = "HarryIsGood"
+d = name[0::3]
+d1: str = name[:0:-1]
+print(d)
